3c9fd2b4347fb5e0e43eb8129a79bfb4689e801a
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-04-22 13:24:49 -0400

    projects model functions and router functions in place

--- api/project/model.js
+++ api/project/model.js
@@ -1,3 +1,17 @@
 // build your `Project` model here
 
 const db = require("../../data/dbConfig");
+
+const getProjects = () => {
+  return db("projects");
+};
+
+const createProject = async (project) => {
+  const [id] = await db("projects").insert(project);
+  return getProjects().where("project_id", id).first();
+};
+
+module.exports = {
+  getProjects,
+  createProject,
+};
--- api/project/router.js
+++ api/project/router.js
@@ -2,8 +2,26 @@
 
 const router = require("express").Router();
 
+const Projects = require("./model");
+
 router.get("/", (req, res, next) => {
-  res.status(200).json("hello");
+  Projects.getProjects()
+    .then((projects) => {
+      res.status(200).json(projects);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.post("/", (req, res, next) => {
+  Projects.createProject(req.body)
+    .then((newProject) => {
+      res.status(201).json(newProject);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 module.exports = router;
