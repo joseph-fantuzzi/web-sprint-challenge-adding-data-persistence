88b52e49388b65d2a6c82a6371d695702d698820
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-04-22 13:35:18 -0400

    tasks model functions and router functions in place

--- api/task/model.js
+++ api/task/model.js
@@ -1,3 +1,17 @@
 // build your `Task` model here
 
 const db = require("../../data/dbConfig");
+
+const getTasks = () => {
+  return db("tasks");
+};
+
+const createTask = async (task) => {
+  const [id] = await db("tasks").insert(task);
+  return getTasks().where("task_id", id).first();
+};
+
+module.exports = {
+  getTasks,
+  createTask,
+};
--- api/task/router.js
+++ api/task/router.js
@@ -2,8 +2,26 @@
 
 const router = require("express").Router();
 
+const Tasks = require("./model");
+
 router.get("/", (req, res, next) => {
-  res.status(200);
+  Tasks.getTasks()
+    .then((tasks) => {
+      res.status(200).json(tasks);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.post("/", (req, res, next) => {
+  Tasks.createTask(req.body)
+    .then((newTask) => {
+      res.status(201).json(newTask);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 module.exports = router;
