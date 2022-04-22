6939bf7aca8941c1a51e6dd73018d9a1ba25c41f
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-04-22 11:52:44 -0400

    express server started

--- api/server.js
+++ api/server.js
@@ -1,1 +1,20 @@
 // build your server here and require it from index.js
+
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+server.use("*", (req, res) => {
+  res.json({ message: "Express server is working." });
+});
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
