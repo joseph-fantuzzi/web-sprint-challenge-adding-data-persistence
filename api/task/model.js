5f438b63d0e6412b46ca42779027a3ce35f2d063
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-04-22 14:21:45 -0400

    added a join to the getTasks model function

--- api/task/model.js
+++ api/task/model.js
@@ -3,12 +3,21 @@
 const db = require("../../data/dbConfig");
 
 const getTasks = () => {
-  return db("tasks");
+  return db("tasks as t")
+    .leftJoin("projects as p", "t.project_id", "p.project_id")
+    .select(
+      "t.task_id",
+      "t.task_description",
+      "t.task_notes",
+      "t.task_completed",
+      "p.project_name",
+      "p.project_description"
+    );
 };
 
 const createTask = async (task) => {
   const [id] = await db("tasks").insert(task);
-  return getTasks().where("task_id", id).first();
+  return db("tasks").where("task_id", id).first();
 };
 
 module.exports = {
